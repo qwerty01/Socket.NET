e67aa15fb2fa6078b510738c60c2ce4f59f287f7
qwerty01 <qw3rty01@gmail.com>
2014-08-09 14:19:29 -0400

    Added send and private functions to GenericSocket
    Send has an asynchronous and synchronous option
    SocketAsyncEventArgs are stored in a pool and reused
    Disposable implemented
    
    TODO:
    Recieve()

--- Socket.NET/GenericSocket.cs
+++ Socket.NET/GenericSocket.cs
@@ -26,12 +26,104 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Socket.NET
 {
-    class GenericSocket
+    class GenericSocket : IDisposable
     {
+        public delegate void SendStringDelegate(string data, SocketError error);
+
+        private Stack<SocketAsyncEventArgs> _eventPool;
+
+        protected System.Net.Sockets.Socket socket;
+        protected int defaultBuffLen;
+
+        public bool Connected { get { return socket.Connected; } }
+
+        protected internal GenericSocket(bool tcp = true, int defaultBuffLen = 256)
+        {
+            socket = (tcp) ?
+                new System.Net.Sockets.Socket(System.Net.Sockets.SocketType.Dgram, System.Net.Sockets.ProtocolType.Udp) :
+                new System.Net.Sockets.Socket(System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
+            _eventPool = new Stack<SocketAsyncEventArgs>();
+            this.defaultBuffLen = defaultBuffLen;
+        }
+
+        public int Send(string data)
+        {
+            // Returns sent data length, -1 on error
+            if (!Connected)
+                return -1;
+            return socket.Send(UnicodeEncoding.UTF8.GetBytes(data));
+        }
+        public int Send(string data, SendStringDelegate callback)
+        {
+            // Returns error code, 0 on success
+            if (!Connected)
+                return (int)SocketError.NotConnected;
+
+            SocketAsyncEventArgs e = getArg(UTF8Encoding.UTF8.GetBytes(data), callback, SendStringCallback);
+            if (socket.SendAsync(e))
+                return (int)e.SocketError; // Send was done asynchronously
+            else
+            {
+                //Send is done synchronously
+                int ret = (int)e.SocketError;
+                callback(data, e.SocketError); // Call the callback
+                recycleArg(e, SendStringCallback);
+                return ret;
+            }
+        }
+
+        public byte[] Recieve()
+        {
+            // Figure out how to do buffer sizes for receive (while loop checking length of Receive()?)
+        }
+
+        protected void SendStringCallback(object sender, SocketAsyncEventArgs e)
+        {
+            SendStringDelegate callback = (SendStringDelegate)e.UserToken;
+            callback(UTF8Encoding.UTF8.GetString(e.Buffer), e.SocketError);
+            recycleArg(e, SendStringCallback);
+        }
+
+        protected SocketAsyncEventArgs getArg(byte[] buffer, SendStringDelegate callback, EventHandler<SocketAsyncEventArgs> handler)
+        {
+            return getArg(buffer, 0, buffer.Length, callback, handler);
+        }
+        protected SocketAsyncEventArgs getArg(byte[] buffer, int count, SendStringDelegate callback, EventHandler<SocketAsyncEventArgs> handler)
+        {
+            return getArg(buffer, 0, count, callback, handler);
+        }
+        protected SocketAsyncEventArgs getArg(byte[] buffer, int offset, int count, SendStringDelegate callback, EventHandler<SocketAsyncEventArgs> handler)
+        {
+            SocketAsyncEventArgs e = (_eventPool.Count == 0) ? new SocketAsyncEventArgs() : _eventPool.Pop();
+            e.SetBuffer(buffer, offset, count);
+            e.UserToken = callback;
+            e.Completed += handler;
+            e.SocketError = SocketError.Success;
+            return e;
+        }
+
+        protected void recycleArg(SocketAsyncEventArgs e, EventHandler<SocketAsyncEventArgs> handler)
+        {
+            e.UserToken = null;
+            e.SetBuffer(null, 0, 0);
+            e.Completed -= handler;
+            _eventPool.Push(e);
+        }
+
+        public void Dispose()
+        {
+            while (_eventPool.Count > 0)
+                _eventPool.Pop().Dispose();
+            if (Connected)
+                socket.Disconnect(false);
+            socket.Close();
+        }
     }
 }
